426e5ddaae7c4cf7330428241f5d8c10c741322a
JulienFink <64968597+JulienFink@users.noreply.github.com>
2021-03-31 21:46:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Hashed_Database_Users
+{
+    class Program
+    {
+        static void welcome(UserDB x)
+        {
+            Console.WriteLine();
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - -");
+            Console.WriteLine("Welcome ! What would you like to do ?");
+            Console.WriteLine();
+            Console.WriteLine("1. Create account");
+            Console.WriteLine("2. Login");
+            Console.WriteLine("3. Leave");
+            Console.WriteLine();
+            Console.WriteLine("Choose one option among those above : ");
+            string choice = Console.ReadLine();
+
+            if(choice == "1")
+            {
+                x.defUser();
+                Environment.Exit(0);
+            }
+            if(choice == "2")
+            {
+                x.login();
+                Environment.Exit(0);
+            }
+            if(choice == "3")
+            {
+                Environment.Exit(0);
+            }            
+        }
+
+        static void Main(string[] args)
+        {
+            UserDB db = new UserDB();
+            welcome(db);            
+        }
+    }
+}
---
+++ UserDB.cs
@@ -0,0 +1,185 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace Hashed_Database_Users
+{
+    class UserDB
+    {
+        public MySqlConnection connection = null;
+
+        #region Ctor
+        public UserDB()
+        {
+            try
+            {
+                string connexionString = "SERVER=****************;PORT=****************;" +
+                                         "DATABASE=****************;" +
+                                         "UID=****************;PASSWORD=****************";
+
+                this.connection = new MySqlConnection(connexionString);
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(" Connection Error : " + e.ToString());
+                return;
+            }
+        }
+        #endregion
+
+        #region ~ void : displays message either authentication is successful or not
+        private void authType(int number)
+        {
+            Console.WriteLine();
+            if (number == 0) { Console.WriteLine("Login successful !"); }
+            if (number == 1) { Console.WriteLine("Authentication failed"); }
+            Console.WriteLine();
+        }
+        #endregion
+
+        #region ~ void : defines a new User
+        public void defUser()
+        {
+            Console.Write("Enter your first name : ");
+            string fname = Console.ReadLine();
+
+            Console.Write("Enter your last name : ");
+            string lname = Console.ReadLine();
+
+            Console.Write("Enter your email : ");
+            string email = Console.ReadLine();
+
+            User k = new User(fname, lname, email);
+            //Defines password for the user
+            k.hashPass();
+
+            //Creates a new User in the database
+            createUser(k);
+        }
+        #endregion
+
+        #region ~ void : creates a new user in the database
+        public void createUser(User k)
+        {
+            try
+            {
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = " INSERT INTO users (id, first_name, last_name, password, mail) "
+                                    + $"VALUES(NULL, '{k.first_name}', '{k.last_name}', '{k.hashed_password}', '{k.email_add}');";
+                command.ExecuteReader();
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Error.");                
+            }
+            finally
+            {
+                connection.Close();
+            }            
+        }
+        #endregion
+
+        #region ~ bool : checks if a (user) mail is in DB
+        public bool checksUser(string mail)
+        {
+            try
+            {
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = " SELECT COUNT(*) FROM users "
+                                    + $"WHERE mail = '{mail}';";
+                MySqlDataReader reader;
+                reader = command.ExecuteReader();
+
+                int value = 0;
+                while(reader.Read())
+                {
+                    value = reader.GetInt32(0);
+                    if(value == 1)
+                    {
+                        connection.Close();
+                        return true;
+                    }                   
+                }
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Error.");
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+        #endregion
+
+        #region ~ bool : login - first part
+        public void login()
+        {
+            Console.Write("Enter your email : ");
+            string mail = Console.ReadLine();
+
+            Console.Write("Enter your password : ");
+            User y = new User(null, null, null);
+            y.hashed_password = "";
+            string hash = y.hashPass();
+
+            Console.WriteLine();
+
+            if (checksUser(mail))
+            {
+                loginUser(mail, hash.ToString());
+            }
+            else
+            {
+                authType(1);
+            }
+        }
+        #endregion
+
+        #region ~ bool : login - second part
+        public bool loginUser(string mail, string hash)
+        {
+            try
+            {
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = " SELECT * FROM users "
+                                    + $"WHERE mail = '{mail}';";
+                MySqlDataReader reader;
+                reader = command.ExecuteReader();
+
+                User z = new User(null, null, null);
+
+                while(reader.Read())
+                {
+                    z.first_name = reader.GetString(1);
+                    z.last_name = reader.GetString(2);
+                    z.hashed_password = reader.GetString(3);
+                    z.email_add = reader.GetString(4);
+
+                    if (z.verifyHash(hash) && z.email_add == mail)
+                    {
+                        authType(0);
+                    }
+                    else
+                    {
+                        authType(1);
+                    }
+                }                             
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Error.");
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+        #endregion
+
+    }
+}
