c78605336db37e66fee723ff870571d0fe7bf12c
JulienFink <64968597+JulienFink@users.noreply.github.com>
2021-12-01 17:36:01 +0100

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Hashed_Database_Users
+{
+    public class User
+    {
+        public string first_name;
+        public string last_name;
+        public string hashed_password = null;
+        public string salt = null;
+        public string email_add;
+
+        #region ~ User : ctor
+        public User(string fname, string lname, string pass, string sel, string mail)
+        {
+            this.first_name = fname;
+            this.last_name = lname;
+            this.hashed_password = pass;
+            this.salt = sel;
+            this.email_add = mail;
+        }
+        #endregion
+
+        #region ~ public string : replaces user's input by stars
+        public string passInput()
+        {
+            var password = string.Empty;
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Escape:
+                        return null;
+                    case ConsoleKey.Enter:
+                        return password;
+                    case ConsoleKey.Backspace:
+                        if (password.Length > 0)
+                        {
+                            password = password.Substring(0, (password.Length - 1));
+                            Console.Write("\b \b");
+                        }
+                        break;
+                    default:
+                        password += key.KeyChar;
+                        Console.Write("*");
+                        break;
+                }
+            }
+        }
+        #endregion
+
+        #region ~ private string : defines a new password for a user (checks length and equality)
+        public void defPass()
+        {
+            while (true)
+            {
+                var pass1 = string.Empty;
+                var pass2 = string.Empty;
+
+                Console.Write("Enter your new password : ");
+                pass1 = passInput();
+                Console.WriteLine();
+
+                Console.Write("Enter your password again : ");
+                pass2 = passInput();
+                Console.WriteLine();
+
+                if (pass1.Length < 8)
+                {
+                    Console.WriteLine("Password must be at least 8 characters long...");
+                    Console.WriteLine();
+                }
+                if (pass1 != pass2)
+                {
+                    Console.WriteLine("Both passwords must be equal...");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    this.hashed_password = pass1;
+                    break;
+                }
+            }
+        }
+        #endregion
+
+        #region ~ void : sets the hashed password for a user
+        public string hashPass()
+        {            
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                //randomly adds salt to a clear password
+                this.salt = RandomString();
+                this.hashed_password = this.hashed_password + this.salt;
+
+                //Hash(password + salt)
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(this.hashed_password));
+
+                StringBuilder builder = new StringBuilder();
+                for (int k = 0; k < bytes.Length; k++)
+                {
+                    // "x2" returns the hexadecimal string
+                    builder.Append(bytes[k].ToString("x2"));
+                }
+                this.hashed_password = builder.ToString();             
+            }
+            return this.hashed_password;
+        }
+        #endregion
+
+        #region ~ string : returns a random salt
+        public string RandomString()
+        {
+            const string chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789&é(-_çà)=,;:/!";
+            var builder = new StringBuilder("");
+
+            for (int k = 0; k < 8; k++)
+            {
+                var rand = new Random();
+                builder.Append( chars[ rand.Next(chars.Length) ] );
+            }
+            return builder.ToString();
+        }
+        #endregion
+
+    }
+}
---
+++ UserDB.cs
@@ -0,0 +1,196 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hashed_Database_Users
+{
+    class UserDB
+    {
+        public MySqlConnection connection = null;
+
+        #region Ctor
+        public UserDB()
+        {
+            try
+            {
+                string connexionString = "SERVER=****************;PORT=****************;" +
+                                        "DATABASE=****************;" +
+                                        "UID=****************;PASSWORD=****************";
+
+                this.connection = new MySqlConnection(connexionString);
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(" Connection Error : " + e.ToString());
+                return;
+            }
+        }
+        #endregion
+
+        #region ~ void : displays message either authentication is successful or not
+        private void authType(int number)
+        {
+            Console.WriteLine();
+            if (number == 0) { Console.WriteLine("Login successful !"); }
+            if (number == 1) { Console.WriteLine("Authentication failed."); }
+            Console.WriteLine();
+        }
+        #endregion
+
+        #region ~ void : defines a new User
+        public void defUser()
+        {
+            Console.Write("Enter your first name : ");
+            string fname = Console.ReadLine();
+
+            Console.Write("Enter your last name : ");
+            string lname = Console.ReadLine();
+
+            Console.Write("Enter your email : ");
+            string email = Console.ReadLine();
+
+            User k = new User(fname, lname, null, null, email);
+            //Defines password for the user
+            k.defPass();
+
+            k.hashPass();
+
+            //Creates a new User in the database
+            createUser(k);
+        }
+        #endregion
+
+        #region ~ void : creates a new user in the database
+        public void createUser(User k)
+        {
+            
+            connection.Open();
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = " INSERT INTO users (id, first_name, last_name, password, salt, mail) "
+                                + $"VALUES(NULL, '{k.first_name}', '{k.last_name}', '{k.hashed_password}', '{k.salt}', '{k.email_add}');";
+            command.ExecuteReader();          
+        }
+        #endregion
+
+        #region ~ bool : checks if a (user) mail is in DB
+        public bool checksUser(string mail)
+        {
+            try
+            {
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = " SELECT COUNT(*) FROM users "
+                                    + $"WHERE mail = '{mail}';";
+                MySqlDataReader reader;
+                reader = command.ExecuteReader();
+
+                int value = 0;
+                while(reader.Read())
+                {
+                    value = reader.GetInt32(0);
+                    if(value == 1)
+                    {
+                        connection.Close();
+                        return true;
+                    }                   
+                }
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Error.");
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+        #endregion
+
+        #region ~ bool : login - first part
+        public void init_login()
+        {
+            Console.Write("Enter your email : ");
+            string mail = Console.ReadLine();
+
+            Console.Write("Enter your password : ");
+            User dummy = new User(null, null, null, null, null);
+            string password = dummy.passInput();
+
+            Console.WriteLine();
+            if (checksUser(mail))
+            {
+                loginUser(mail, password);
+            }
+            else
+            {
+                authType(1);
+            }
+        }
+        #endregion
+
+        #region ~ void : login - second part
+        public void loginUser(string mail, string password)
+        {
+            try
+            {
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = " SELECT password, salt FROM users "
+                                    + $"WHERE mail = '{mail}';";
+                MySqlDataReader reader;
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string hashed_password1 = reader.GetString(0);
+                    string salt1 = reader.GetString(1);
+
+                    string hashed = hash(password, salt1);
+
+                    if (hashed_password1 == hashed)
+                    {
+                        authType(0);
+                    }
+                    else
+                    {
+                        authType(1);
+                    }
+                }
+            }              
+            catch (MySqlException)
+            {
+                Console.WriteLine("Error.");
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        #endregion
+
+        #region ~ void : computes the hash from
+        public string hash(string password, string salt)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                string to_be_hashed = password + salt;
+
+                //Hash(password + salt)
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(to_be_hashed));
+
+                StringBuilder builder = new StringBuilder();
+                for (int k = 0; k < bytes.Length; k++)
+                {
+                    // "x2" returns the hexadecimal string
+                    builder.Append(bytes[k].ToString("x2"));
+                }
+                string hashed = builder.ToString();
+                return hashed;
+            }            
+        }
+        #endregion
+
+    }
+}
